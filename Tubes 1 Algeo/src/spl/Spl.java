ba2c149ee1f676e17d1b4c030b8b6386f1c9da69
Vivianni <Vivianni@hixalyn>
2018-09-26 21:05:38 +0700

    commit by vivianni

--- Tubes 1 Algeo/src/spl/Spl.java
+++ Tubes 1 Algeo/src/spl/Spl.java
@@ -1,18 +1,33 @@
 package spl;
 import matrix.Matrix;
 
-public class Spl
+public class Spl extends matrix.Matrix
 {
 
-	public static void main(String[] args)
+	public void cariSolusiUnik()
+	//Menuliskan semua solusi unik dari matriks ke dalam suatu array
 	{
-		Matrix data = new Matrix(3,3);
-		// TODO Auto-generated method stub
-		System.out.println("Hello World"); //prints hello world
-		data.BacaMatrix(3,3);
-		//data.TulisMATRIKS();
-		System.out.println("Determinan= " + data.Determinan());
-		data.TulisMATRIKS();
+		int i;
+		double [] solusi = new double [this.GetLastIdxKol()-1];
+		
+		//Mengurang ruas kanan persamaan dengan ruas kiri
+		for (i=this.GetLastIdxBrs();i>=this.GetFirstIdxBrs();i--)
+		{	
+			for (int j=this.GetFirstIdxKol();j>=this.GetLastIdxKol()-1;j++)
+			{
+				solusi[i]=solusi[i]-this.Elmt(i,j)*solusi[j];
+			}
+			solusi[i]=solusi[i]+this.Elmt(i,this.GetLastIdxKol());
+		}
+		
+		//print solusi
+		for (i=this.GetFirstIdxKol();i<=this.GetLastIdxKol();i++)
+		{
+			System.out.println("X" + i + " = " + solusi[i]);
+		}
 	}
+	
+	
+	
 
 }
